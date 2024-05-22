c000f6b5298d8e1b3cd61f44722c771b60b3d29a
Jordy Potman <jordy@synopsys.com>
2024-05-22 10:26:36 +0200

    Enable termguicolors before setting nvim colorscheme

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -5,6 +5,7 @@ return {
     lazy = false, -- Make sure main colorscheme is loaded during starup.
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
+      vim.opt.termguicolors = true
       vim.cmd([[colorscheme base16-tomorrow-night]])
     end,
   },
