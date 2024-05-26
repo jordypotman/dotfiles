c6bdc45541e72aa729890b4bcf5ae23e3a2b02f5
Jordy Potman <jordypotman@gmail.com>
2024-05-26 18:35:38 +0200

    Customize nvim lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,12 +3,33 @@ local Plugin = { 'nvim-lualine/lualine.nvim' }
 
 Plugin.event = 'VeryLazy'
 
+local function encoding_fileformat()
+  return vim.opt.fileencoding:get() .. '[' .. vim.bo.fileformat .. ']'
+end
+
+local function location()
+  local line = vim.fn.line('.')
+  local num_lines = tostring(vim.fn.line('$'))
+  local len_num_lines = tostring(string.len(num_lines))
+  local col = vim.fn.virtcol('.')
+  return string.format('%' .. len_num_lines .. 'd/%s:%-2d', line, num_lines, col)
+end
+
+local common_sections = {
+  lualine_c = { { 'filename', path = 1 } },
+  lualine_x = { 'filetype' },
+  lualine_y = { encoding_fileformat },
+  lualine_z = { '%3p%%', location }
+}
+
 Plugin.opts = {
   options = {
     icons_enabled = true,
     section_separators = '',
     component_separators = ''
-  }
+  },
+  sections = common_sections,
+  inactive_sections = common_sections,
 }
 
 function Plugin.init()
