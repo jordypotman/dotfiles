3872e545b0291f37c8209ccdbc50f855703d44df
Jordy Potman <jordypotman@gmail.com>
2024-06-01 18:39:49 +0200

    Add commands for controlling display of nvim diagnostics

--- .config/nvim/lua/user/commands.lua
+++ .config/nvim/lua/user/commands.lua
@@ -7,6 +7,35 @@ function commands.setup()
       vim.diagnostic.enable(not vim.diagnostic.is_enabled())
     end, {})
 
+  vim.api.nvim_create_user_command('DiagnosticsUnderlineToggle',
+    function()
+      vim.diagnostic.config({ underline = not vim.diagnostic.config().underline });
+    end, {})
+
+  vim.api.nvim_create_user_command('DiagnosticsVirtualTextToggle',
+    function()
+      vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text });
+    end, {})
+
+  local function get_min_severity(opts)
+    return { severity = { min = opts.fargs[1] } }
+  end
+  local function complete_diagnostic_severity()
+    return { unpack(vim.diagnostic.severity) }
+  end
+
+  vim.api.nvim_create_user_command('DiagnosticsUnderlineMinSeverity',
+    function(opts)
+      vim.diagnostic.config({ underline = get_min_severity(opts) })
+    end,
+    { nargs = 1, complete = complete_diagnostic_severity })
+
+  vim.api.nvim_create_user_command('DiagnosticsVirtualTextMinSeverity',
+    function(opts)
+      vim.diagnostic.config({ virtual_text = get_min_severity(opts) })
+    end,
+    { nargs = 1, complete = complete_diagnostic_severity })
+
   -- Make focussed window more obvious by starting the colorcolumn from
   -- column 1 and disabling the cursorline in unfocussed windows.
   -- Based on: https://github.com/wincent/wincent/blob/2aa44544fe9e8fc466fea42391e66fff3583054c/roles/dotfiles/files/.vim/plugin/autocmds.vim
