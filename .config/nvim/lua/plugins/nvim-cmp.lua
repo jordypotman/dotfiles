164204c205cf5a51393bbe82326b10b254f77044
Jordy Potman <jordypotman@gmail.com>
2025-03-02 16:07:09 +0100

    [nvim-cmp] Confirm with Tab when there's only one completion entry

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -26,7 +26,27 @@ function Plugin.config()
     },
     formatting = cmp_format,
     mapping = cmp.mapping.preset.insert({
-      ['<Tab>'] = cmp_action.tab_complete(),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+        if cmp.visible() then
+          if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
+          else
+            cmp.select_next_item()
+          end
+        elseif has_words_before() then
+          cmp.complete()
+          if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
+          end
+        else
+          fallback()
+        end
+      end, {'i', 's'}),
       ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
       ['<CR>'] = cmp.mapping.confirm()
     })
