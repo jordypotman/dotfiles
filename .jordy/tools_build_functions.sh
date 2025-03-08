515ec73b37277c92afcdae3f46d3865958ca6285
Jordy Potman <jordy@synopsys.com>
2025-03-08 19:33:30 +0100

    [.jordy] Add .jordy/tools_build_functions.sh script
    Currently it only contains a function, to download, build and install
    Neovim.

---
+++ .jordy/tools_build_functions.sh
@@ -0,0 +1,28 @@
+jordy_get_tools_dir() {
+  echo "/SCRATCH/jordy/tools"
+}
+
+jordy_download_build_install_neovim() {
+  # https://github.com/neovim/neovim/archive/refs/tags/v0.10.4.tar.gz
+  NEOVIM_SRC_URL="https://github.com/neovim/neovim/archive/refs/tags/"
+  NEOVIM_VERSION="0.10.4"
+
+  tools_dir=$(jordy_get_tools_dir)
+  tools_neovim_dir="${tools_dir}/neovim"
+  archive_name="v${NEOVIM_VERSION}.tar.gz"
+  dir_name="neovim-${NEOVIM_VERSION}"
+  install_prefix="${tools_neovim_dir}/installs/${dir_name}"
+
+  mkdir -p "${tools_neovim_dir}" && \
+  pushd "${tools_neovim_dir}" > /dev/null && \
+  ([ -f "${archive_name}" ] || wget "${NEOVIM_SRC_URL}${archive_name}") && \
+  ([ -d "${dir_name}" ] || tar zxf "${archive_name}") && \
+  cd "${dir_name}" && \
+  ([ -d "${install_prefix}" ] || CC=$(which gcc) make CMAKE_BUILD_TYPE=Release \
+  CMAKE_INSTALL_PREFIX="${install_prefix}" install) && \
+  cd "${install_prefix}" && \
+  ([ -L "lib" ] || ln -s lib64 lib) && \
+  cd .. && \
+  ln -sfn "${dir_name}" neovim && \
+  popd > /dev/null
+}
