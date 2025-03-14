944eb4db03d31ca44342e48eff0c407606144fbb
Jordy Potman <jordy@synopsys.com>
2025-03-14 18:52:58 +0100

    [nvim] Add trouble.nvim plugin

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,7 @@
+-- A pretty diagnostics, references, telescope results, quickfix, and location
+-- list to help you solve all the trouble your code is causing.
+return {
+  'folke/trouble.nvim',
+  opts = {},
+  cmd = 'Trouble'
+}
