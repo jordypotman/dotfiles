1a16801d6301258233ae1d7ca651bbe2cde19b36
Jordy Potman <jordy@synopsys.com>
2025-03-14 18:17:48 +0100

    [nvim] Refactor lualine plugin spec to return a table

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,39 +1,40 @@
 -- NeoVim statusline.
-local Plugin = { 'nvim-lualine/lualine.nvim' }
+return {
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
 
-Plugin.event = 'VeryLazy'
+  opts = function()
+    local function encoding_fileformat()
+      return vim.opt.fileencoding:get() .. '[' .. vim.bo.fileformat .. ']'
+    end
 
-local function encoding_fileformat()
-  return vim.opt.fileencoding:get() .. '[' .. vim.bo.fileformat .. ']'
-end
+    local function location()
+      local line = vim.fn.line('.')
+      local num_lines = tostring(vim.fn.line('$'))
+      local len_num_lines = tostring(string.len(num_lines))
+      local col = vim.fn.virtcol('.')
+      return string.format('%' .. len_num_lines .. 'd/%s:%-2d', line, num_lines, col)
+    end
 
-local function location()
-  local line = vim.fn.line('.')
-  local num_lines = tostring(vim.fn.line('$'))
-  local len_num_lines = tostring(string.len(num_lines))
-  local col = vim.fn.virtcol('.')
-  return string.format('%' .. len_num_lines .. 'd/%s:%-2d', line, num_lines, col)
-end
+    local common_sections = {
+      lualine_c = { { 'filename', path = 1 } },
+      lualine_x = { 'filetype' },
+      lualine_y = { encoding_fileformat },
+      lualine_z = { '%3p%%', location }
+    }
 
-local common_sections = {
-  lualine_c = { { 'filename', path = 1 } },
-  lualine_x = { 'filetype' },
-  lualine_y = { encoding_fileformat },
-  lualine_z = { '%3p%%', location }
+    return {
+      options = {
+        icons_enabled = true,
+        section_separators = '',
+        component_separators = ''
+      },
+      sections = common_sections,
+      inactive_sections = common_sections,
+    }
+  end,
+
+  init = function()
+    vim.opt.showmode = false
+  end
 }
-
-Plugin.opts = {
-  options = {
-    icons_enabled = true,
-    section_separators = '',
-    component_separators = ''
-  },
-  sections = common_sections,
-  inactive_sections = common_sections,
-}
-
-function Plugin.init()
-  vim.opt.showmode = false
-end
-
-return Plugin
