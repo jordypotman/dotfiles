481b5ffe6de229b1a7de21b9dd7cc69f98b3384a
Jordy Potman <jordy@synopsys.com>
2025-08-01 17:41:01 +0200

    [nvim] Add rainbow_csv.nvim plugin

---
+++ .config/nvim/lua/plugins/csv.lua
@@ -0,0 +1,24 @@
+return {
+  -- Neovim port of mechatroner/rainbow_csv
+  -- Highlight columns in CSV and TSV files and run queries in SQL-like
+  -- language.
+  {
+    'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+      'csv',
+      'tsv',
+      'csv_semicolon',
+      'csv_whitespace',
+      'csv_pipe',
+      'rfc_csv',
+      'rfc_semicolon'
+    },
+    cmd = {
+      'RainbowDelim',
+      'RainbowDelimSimple',
+      'RainbowDelimQuoted',
+      'RainbowMultiDelim'
+    }
+  }
+}
