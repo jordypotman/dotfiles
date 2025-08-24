1b0ba8c0a324561188322191a9a449115a2e3663
Jordy Potman <jordypotman@gmail.com>
2025-08-24 12:58:37 +0200

    [nvim] Use vim.o instead of vim.opt to set options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,41 +2,41 @@ local options = {}
 
 function options.setup()
   -- Enable line numbering.
-  vim.opt.number = true
+  vim.o.number = true
 
   -- Enable the use of the mouse in all modes.
-  vim.opt.mouse = 'a'
+  vim.o.mouse = 'a'
 
   -- Ignore case in search patterns unless the pattern contains upper case
   -- characters.
-  vim.opt.ignorecase = true
-  vim.opt.smartcase = true
+  vim.o.ignorecase = true
+  vim.o.smartcase = true
 
   -- Disable highlighting all matches of a previous search pattern.
-  vim.opt.hlsearch = false
+  vim.o.hlsearch = false
 
   -- Turn wrapping of lines longer than the width of the window off.
-  vim.opt.wrap = false
+  vim.o.wrap = false
 
   -- Always draw the signcolumn.
-  vim.opt.signcolumn = 'yes'
+  vim.o.signcolumn = 'yes'
 
   -- Open new split panes to the right of or below the current pane.
-  vim.opt.splitright = true
-  vim.opt.splitbelow = true
+  vim.o.splitright = true
+  vim.o.splitbelow = true
 
   -- Complete till longest common string, showing all matches in wildmenu on
   -- first press of <Tab>, complete the next full match on second press of <Tab>.
   -- See: http://stackoverflow.com/questions/526858
-  vim.opt.wildmode = 'longest:full,full'
+  vim.o.wildmode = 'longest:full,full'
 
   -- Start scrolling when the cursor is one line from the top or bottom of the
   -- window.
-  vim.opt.scrolloff = 1
+  vim.o.scrolloff = 1
 
   -- Start scrolling when the cursor is one column from the left or right of the
   -- windows.
-  vim.opt.sidescrolloff=2
+  vim.o.sidescrolloff = 2
 
   -- Sort diagnostics by severity so signs and virtual text display the
   -- diagnostics with the highest severity.
