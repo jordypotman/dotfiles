4383c4e5b0c3a11169866d8dafcf4c880c71469f
Jordy Potman <jordy@synopsys.com>
2025-10-31 18:33:08 +0100

    [nvim] Add snacks.nvim util plugin

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,3 +1,13 @@
 return {
-  { "nvim-lua/plenary.nvim", lazy = true }
+  { "nvim-lua/plenary.nvim", lazy = true },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      input = { enabled = true },
+      picker = { enabled = true },
+    }
+  }
 }
